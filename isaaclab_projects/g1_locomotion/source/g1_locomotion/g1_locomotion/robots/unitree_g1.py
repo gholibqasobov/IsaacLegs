c76960b36af5bffa573fc8a133d43a7b4686568e
qasob <qasobovgholib@gmail.com>
2026-06-18 11:36:02 +0500

    adding g1 29dof config

--- isaaclab_projects/g1_locomotion/source/g1_locomotion/g1_locomotion/robots/unitree_g1.py
+++ isaaclab_projects/g1_locomotion/source/g1_locomotion/g1_locomotion/robots/unitree_g1.py
@@ -1,5 +1,5 @@
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.actuators import ImplicitActuatorCfg, DCMotorCfg
 from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
 
@@ -109,4 +109,157 @@ G1_CFG = ArticulationCfg(
         ),
     },
 )
+
+
+G1_29DOF_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"src/robots/g1_description/usd/g1_29dof_rev_1_0/g1_29dof_rev_1_0/g1_29dof_rev_1_0.usd",
+        activate_contact_sensors=False,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            fix_root_link=False,  # Configurable - can be set to True for fixed base
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=4,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.75),
+        rot=(0.7071, 0, 0, 0.7071),
+        joint_pos={
+            ".*_hip_pitch_joint": -0.10,
+            ".*_knee_joint": 0.30,
+            ".*_ankle_pitch_joint": -0.20,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "legs": DCMotorCfg(
+            joint_names_expr=[
+                ".*_hip_yaw_joint",
+                ".*_hip_roll_joint",
+                ".*_hip_pitch_joint",
+                ".*_knee_joint",
+            ],
+            effort_limit={
+                ".*_hip_yaw_joint": 88.0,
+                ".*_hip_roll_joint": 88.0,
+                ".*_hip_pitch_joint": 88.0,
+                ".*_knee_joint": 139.0,
+            },
+            velocity_limit={
+                ".*_hip_yaw_joint": 32.0,
+                ".*_hip_roll_joint": 32.0,
+                ".*_hip_pitch_joint": 32.0,
+                ".*_knee_joint": 20.0,
+            },
+            stiffness={
+                ".*_hip_yaw_joint": 100.0,
+                ".*_hip_roll_joint": 100.0,
+                ".*_hip_pitch_joint": 100.0,
+                ".*_knee_joint": 200.0,
+            },
+            damping={
+                ".*_hip_yaw_joint": 2.5,
+                ".*_hip_roll_joint": 2.5,
+                ".*_hip_pitch_joint": 2.5,
+                ".*_knee_joint": 5.0,
+            },
+            armature={
+                ".*_hip_.*": 0.03,
+                ".*_knee_joint": 0.03,
+            },
+            saturation_effort=180.0,
+        ),
+        "feet": DCMotorCfg(
+            joint_names_expr=[".*_ankle_pitch_joint", ".*_ankle_roll_joint"],
+            stiffness={
+                ".*_ankle_pitch_joint": 20.0,
+                ".*_ankle_roll_joint": 20.0,
+            },
+            damping={
+                ".*_ankle_pitch_joint": 0.2,
+                ".*_ankle_roll_joint": 0.1,
+            },
+            effort_limit={
+                ".*_ankle_pitch_joint": 50.0,
+                ".*_ankle_roll_joint": 50.0,
+            },
+            velocity_limit={
+                ".*_ankle_pitch_joint": 37.0,
+                ".*_ankle_roll_joint": 37.0,
+            },
+            armature=0.03,
+            saturation_effort=80.0,
+        ),
+        "waist": ImplicitActuatorCfg(
+            joint_names_expr=[
+                "waist_.*_joint",
+            ],
+            effort_limit={
+                "waist_yaw_joint": 88.0,
+                "waist_roll_joint": 50.0,
+                "waist_pitch_joint": 50.0,
+            },
+            velocity_limit={
+                "waist_yaw_joint": 32.0,
+                "waist_roll_joint": 37.0,
+                "waist_pitch_joint": 37.0,
+            },
+            stiffness={
+                "waist_yaw_joint": 5000.0,
+                "waist_roll_joint": 5000.0,
+                "waist_pitch_joint": 5000.0,
+            },
+            damping={
+                "waist_yaw_joint": 5.0,
+                "waist_roll_joint": 5.0,
+                "waist_pitch_joint": 5.0,
+            },
+            armature=0.001,
+        ),
+        "arms": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_shoulder_pitch_joint",
+                ".*_shoulder_roll_joint",
+                ".*_shoulder_yaw_joint",
+                ".*_elbow_joint",
+                ".*_wrist_.*_joint",
+            ],
+            effort_limit=300,
+            velocity_limit=100,
+            stiffness=3000.0,
+            damping=10.0,
+            armature={
+                ".*_shoulder_.*": 0.001,
+                ".*_elbow_.*": 0.001,
+                ".*_wrist_.*_joint": 0.001,
+            },
+        ),
+        "hands": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_index_.*",
+                ".*_middle_.*",
+                ".*_thumb_.*",
+            ],
+            effort_limit=300,
+            velocity_limit=100,
+            stiffness=20,
+            damping=2,
+            armature=0.001,
+        ),
+    },
+    prim_path="/World/envs/env_.*/Robot",
+)
+
+
 """Configuration for the Unitree G1 Humanoid robot."""
